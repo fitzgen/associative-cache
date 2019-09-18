8eb00532b7fa5acaa14d6209a7968802a84189cf
Nick Fitzgerald <fitzgen@gmail.com>
2019-09-18 16:35:59 -0700

    Bench both hash-based and pointer-based `Index` impls

--- crates/test-utils/benches/benches.rs
+++ crates/test-utils/benches/benches.rs
@@ -1,19 +1,22 @@
 use associative_cache::*;
 use criterion::*;
 
-fn run_bench<C: Capacity, I: Indices<usize, C>>(c: &mut Criterion, name: &str) {
+fn run_bench<C: Capacity, I: Indices<*mut u64, C>>(c: &mut Criterion, name: &str) {
     let elems = C::CAPACITY;
 
     {
         let mut group = c.benchmark_group("Insertion");
 
         group.bench_function(name, |b| {
-            let mut cache = AssociativeCache::<usize, usize, C, I, RoundRobinReplacement>::default();
-            let mut iter = (0..elems).cycle();
+            let mut cache =
+                AssociativeCache::<*mut u64, usize, C, I, RoundRobinReplacement>::default();
+            let mut iter = (0..elems)
+                .cycle()
+                .map(|i| (i * std::mem::align_of::<u64>()) as *mut u64);
             b.iter(|| {
                 let i = iter.next().unwrap();
                 let key = black_box(i);
-                let val = black_box(i);
+                let val = black_box(i as usize);
                 black_box(cache.insert(key, val));
             })
         });
@@ -23,16 +26,19 @@ fn run_bench<C: Capacity, I: Indices<usize, C>>(c: &mut Criterion, name: &str) {
         let mut group = c.benchmark_group("Query");
 
         group.bench_function(name, |b| {
-            let mut cache = AssociativeCache::<usize, usize, C, I, RoundRobinReplacement>::default();
+            let mut cache =
+                AssociativeCache::<*mut u64, usize, C, I, RoundRobinReplacement>::default();
 
             for i in 0..elems {
                 // Make the cache have a mix of existing and missing entries.
                 if i % 2 == 0 {
-                    cache.insert(i, i);
+                    cache.insert((i * std::mem::align_of::<u64>()) as *mut u64, i);
                 }
             }
 
-            let mut iter = (0..elems).cycle();
+            let mut iter = (0..elems)
+                .cycle()
+                .map(|i| (i * std::mem::align_of::<u64>()) as *mut u64);
 
             b.iter(|| {
                 let i = iter.next().unwrap();
@@ -56,12 +62,19 @@ macro_rules! define_benches {
 }
 
 define_benches! {
-    direct_mapped_512(Capacity512, DirectMapped);
-    two_way_512(Capacity512, TwoWay);
-    four_way_512(Capacity512, FourWay);
-    eight_way_512(Capacity512, EightWay);
-    sixteen_way_512(Capacity512, SixteenWay);
-    thirty_two_way_512(Capacity512, ThirtyTwoWay);
+    hash_direct_mapped_512(Capacity512, HashDirectMapped);
+    hash_two_way_512(Capacity512, HashTwoWay);
+    hash_four_way_512(Capacity512, HashFourWay);
+    hash_eight_way_512(Capacity512, HashEightWay);
+    hash_sixteen_way_512(Capacity512, HashSixteenWay);
+    hash_thirty_two_way_512(Capacity512, HashThirtyTwoWay);
+
+    pointer_direct_mapped_512(Capacity512, PointerDirectMapped);
+    pointer_two_way_512(Capacity512, PointerTwoWay);
+    pointer_four_way_512(Capacity512, PointerFourWay);
+    pointer_eight_way_512(Capacity512, PointerEightWay);
+    pointer_sixteen_way_512(Capacity512, PointerSixteenWay);
+    pointer_thirty_two_way_512(Capacity512, PointerThirtyTwoWay);
 }
 
 criterion_main!(benches);
